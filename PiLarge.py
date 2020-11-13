044f2da265c34e9090a59cc4589b5c2ac3cbed55
vrmadath <*Vkwp1202sn>
2020-11-13 18:24:20 -0500

    Multiple searches for piLarge

--- PiLarge.py
+++ PiLarge.py
@@ -103,22 +103,66 @@ def server_compute(server_number, message_board, server_table_1, server_table_2,
 
 
 
-def receive_helper(start, end, server_row_1,server_row_2,num_signals):
-	ans = -1
-	while start <= end:
-		mid = (start + end)//2
-		if server_row_1[mid] - server_row_2[mid] == num_signals:
-			ans = mid
-			end = mid - 1
-		else: 
-			start = mid + 1
-	return ans
+def receive_helper(lo, hi, server_row_1,server_row_2,num_signals,signal_indices):
+	# ans = -1
+	# init_lo = start
+	# init_hi = end
+	# while start <= end:
+	# 	mid = (start + end)//2
+	# 	if server_row_1[mid] - server_row_2[mid] == num_signals:
+	# 		ans = mid
+	# 		end = mid - 1
+	# 	else: 
+	# 		start = mid + 1
+	# signal_indices.append(ans)
+	# num_signals = (server_row_1[ans-1] - server_row_2[ans-1]) - (server_row_1[init_lo] - server_row_2[init_lo]) 
+	# if num_signals != 0:
+	# 	receive_helper(init_lo, ans-1,server_row_1,server_row_2, num_signals,signal_indices  )
+	# num_signals = (server_row_1[init_hi] - server_row_2[init_hi]) - (server_row_1[ans + 1] - server_row_2[ans + 1]) 
+	# if num_signals != 0:
+	# 	receive_helper(ans+1, init_hi,server_row_1,server_row_2, num_signals,signal_indices )
+	# print("hi",hi)
+	# print(lo)
+	if hi <= lo :
+		# print("no more signals here")
+		return 
+
+	# print(hi)
+	# print(lo)
+
+	num_signals = (server_row_1[hi] - server_row_2[hi]) - (server_row_1[lo] - server_row_2[lo]) 
+	if num_signals == 0:
+		# print ("NO MORE SIGNALS")
+		return
+	else:   
+		init_lo = lo
+		init_hi = hi
+        # print (lo)
+        # print (hi)
+		while lo <= hi:
+			mid = (lo + hi)//2
+            # print (mid)
+			if (server_row_1[mid] - server_row_2[mid]) - (server_row_1[init_lo] - server_row_2[init_lo])   == num_signals:
+                # print("here")
+                # ans = mid
+				hi = mid - 1
+			else: 
+				lo = mid +1
+
+		# print(lo)
+		signal_indices.append(lo)
+		if lo > 0 and (server_row_1[lo-1] - server_row_2[lo-1]) - (server_row_1[init_lo] - server_row_2[init_lo]) > 0:	
+			receive_helper(init_lo, lo - 1,server_row_1,server_row_2, num_signals, signal_indices)
+		elif lo < len(server_row_1)-1 and (server_row_1[init_hi] - server_row_2[init_hi]) - (server_row_1[lo+1 ] - server_row_2[lo+1]) > 0:
+			receive_helper(lo + 1, init_hi,server_row_1,server_row_2, num_signals, signal_indices) 
+		return
 def receive(server_row_1, server_row_2):
 	num_signals = server_row_1[-1] - server_row_2[-1] 
 	signal_indices = []
 
-	signal_index = receive_helper(0, len(server_row_1), server_row_1,server_row_2, num_signals )
-	return signal_index
+	receive_helper(0, len(server_row_1)-1, server_row_1,server_row_2, num_signals,signal_indices )
+	print(signal_indices)
+	# return signal_indices
 
 
 if __name__ == "__main__":
@@ -146,15 +190,18 @@ if __name__ == "__main__":
 
 		toc = time.perf_counter()
 		sending = (toc - tic)/messages * 1000
+
 		print("Message sending complete . . .\n\n\n")
 		# print(server_table_1)
 		print("Servers computing their tables")
+		tic = time.perf_counter()
+
 		server_table_1 = server_compute(0,message_board, server_table_1, server_table_2,receivers)
 		server_table_2 = server_compute(1,message_board, server_table_1, server_table_2,receivers)
 		# print(server_table_1)
 		print("Servers computing their tables done")
-
-
+		toc = time.perf_counter()
+		server_time = (toc - tic)/messages * 1000
 		tic = time.perf_counter()
 		for i in range(receivers):
 			srow1 = []
@@ -169,7 +216,8 @@ if __name__ == "__main__":
 		receiving = (toc - tic) / receivers *1000
 
 		print("Message receiving complete . . .\n\n\n")
-		print("Average sending time: " + str(sending) + " miliseconds")
-		print("Average receiving time: " + str(receiving) + " miliseconds")
+		print("Average sending time: " + str(sending) + " milliseconds")
+		print("Average receiving time: " + str(receiving) + " milliseconds")
+		print("Average server computation time : " +  str(server_time) + "milliseconds")
 
 
